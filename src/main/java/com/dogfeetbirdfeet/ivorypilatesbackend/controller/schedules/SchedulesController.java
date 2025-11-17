43094247e8f70344c2328a52f78c1ecad4ed2838
newgarden95 <tlsghkdbjs@gmail.com>
2025-11-17 23:28:08 +0900

    스케줄 컨트롤러 생성

---
+++ src/main/java/com/dogfeetbirdfeet/ivorypilatesbackend/controller/schedules/SchedulesController.java
@@ -0,0 +1,14 @@
+package com.dogfeetbirdfeet.ivorypilatesbackend.controller.schedules;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author swo
+ * @apiNote Schedules Controller, 스케쥴 관련 기능에 대한 엔드포인트
+ */
+@RestController
+@RequestMapping("/schedules")
+public class SchedulesController {
+
+}
